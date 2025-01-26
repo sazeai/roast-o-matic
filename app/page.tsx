3280640a23651decfe26b18d8203ebd176ec9088
sazeai <the6thsense137@gmail.com>
2025-01-26 14:22:06 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,19 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-center">
+         <a
+          href="https://www.producthunt.com/posts/roast-o-matic?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-roast&#0045;o&#0045;matic"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=820732&theme=dark&t=1737881218290"
+            alt="Roast&#0045;O&#0045;Matic - Roast&#0045;O&#0045;matic&#0032;&#0124;&#0032;get&#0032;roasted&#0032;faster&#0032;than&#0032;your&#0032;wi&#0045;fi | Product Hunt"
+            style={{ width: '200px', height: '43px' }}
+            width="200"
+            height="43"
+          />
+        </a>
         <RoastGenerator />
       </div>
       <script
